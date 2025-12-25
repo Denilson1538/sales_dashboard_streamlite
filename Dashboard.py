ee15ee9749a4e0db2bada4b1a002bc55f0e28523
Denilson Almeida Lira <denilson1538@hotmail.com>
2025-12-25 09:21:26 -0300

    style: Criacao de duas colunas para armazenar a receita e a quantidade de vendas com suas formatacoes

--- Dashboard.py
+++ Dashboard.py
@@ -3,10 +3,28 @@ import requests
 import pandas as pd
 import plotly.express as px
 
-st.title("DASHBOARD DE MONITORAMENTO DE VENDAS :shopping_cart:")
+def formatar_numero(valor, prefixo=''):
+    for unidade in ['','mil']:
+        if valor <1000:
+            return f'{prefixo}{valor:.2f} {unidade}'
+        valor /=1000
+    return f'{prefixo}{valor:.2f} Milhoes'
+
+st.title('DASHBOARD DE VENDAS :shopping_cart:')
 
 url = 'https://labdados.com/produtos'
 response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 
+coluna1 , coluna2 = st.columns(2)
+with coluna1:
+    st.metric('Receita',formatar_numero(dados['preco'].sum()), 'R$')
+with coluna2:
+    st.metric('quantidade de vendas',formatar_numero(dados.shape[0]))
+
+
+
+
+
 st.dataframe(dados)
+
